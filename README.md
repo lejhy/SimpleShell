fcd555bb2e471a709fa9d3190323f2cf2a9b5d78
lejhy <flejhanec@seznam.cz>
2017-02-04 13:14:56 +0000

    Set up the main loop with helper functions

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-
 CS210 - Simple Shell Assignment
--- main.c
+++ main.c
@@ -3,12 +3,45 @@
 
 #define BUFF_SIZE 512
 
-int main()
+//List of built in commands in the shell
+//If the command is not here, it is either an external program or an error
+char *commands;
+//Pointers to those built in commands
+void (*functions[]) (int argc, char **argv);
+
+//Returns the index of
+int getCommandIndex();
+
+//Returns all arguments in an array of strings
+//Last pointer of the array in set to null
+//If there are no arguments, only the last null pointer is returned
+char **tokenize(char* string);
+void execute();
+
+
+
+
+int main(int argc, char **argv)
 {
     char buffer[BUFF_SIZE];
+    char **tokens;
+    int functionIndex;
+    int i;
+
     while(1){
         printf(">");
         gets(buffer);
+        tokens = tokenize(buffer);
+        i = 0;
+        while (tokens[i] != null){
+        	i++;
+        }
+        if (i > 0){
+        	commandIndex = getCommandIndex(tokens[0]);
+        	if (commandIndex >= 0){
+        		(functions[commandIndex]) (i - 1, tokens+sizeof (tokens));
+        	}
+        }
     }
     return 0;
 }
